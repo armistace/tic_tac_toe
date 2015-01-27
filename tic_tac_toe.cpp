f118cc6fed5b9f2c64c07e11d6f4e1661e6c7a96
armistace <armistace@riseup.net>
2015-01-28 08:36:11 +1000

    Began Unbeatable function and fixed compiler errors

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 using namespace std;
 
 //This function will handle the input from the user
@@ -16,7 +17,7 @@ int choose_position()
 //This function will draw the board on the screen for the user to see what is available
 void draw_board(char current_choices[8])
 {
-    cout <<"\n"
+    cout <<"\n";
     cout << current_choices[0] << " " << current_choices[1] << " " << current_choices[2] << "\n";
     cout << current_choices[3] << " " << current_choices[4] << " " << current_choices[5] << "\n";
     cout << current_choices[6] << " " << current_choices[7] << " " << current_choices[8] << "\n";
@@ -27,6 +28,7 @@ void draw_board(char current_choices[8])
 bool win_condition(char current_board[8])
 {
     bool won;
+    
     if (current_board[0] == current_board[1] && current_board[0] == current_board[2] && current_board[1] == current_board[2])
     {
         won = true;
@@ -63,13 +65,22 @@ bool win_condition(char current_board[8])
     {
         won = false;
     }
+    
+    return won;
 }
 
 //function to check if position is taken
 //It will return true IF THE POSITION IS TAKEN!
-bool position_taken (current_board[8], int choice)
+//Always remember that the function takes in 1 - 9 as the board is drawn
+//not 0 - 8 as the array is referenced!
+bool position_taken (char current_board[8], int choice)
 {
-    if (current_board[choice = 1] != ".")
+    
+    char check = '.';
+    int position = choice -1;
+    
+
+    if (current_board[position] != check)
     {
 	return true;
     }
@@ -80,7 +91,7 @@ bool position_taken (current_board[8], int choice)
 }
 
 //Random AI
-//Generates a random number between 0 and 8 and returns it
+//Generates a random number between 1 and 9 and returns it
 int random_ai()
 {
     int x;
@@ -90,11 +101,11 @@ int random_ai()
     //Generate Random number between 0 and 9
     x = rand() % 10;
     
-    //This ensures that 9 is discarded as it does not make sense on the board
-    while (x == 9)
+    //This ensures that 0 is discarded as it does not make sense on the board
+    while (x == 0)
     {
 	//Need to generate a new seed each time to ensure a different number
-	//if 9 is chosen above
+	//if 0 is chosen above
 	srand(time(NULL));
 	x = rand() % 10;
     }
@@ -105,6 +116,33 @@ int random_ai()
     
 
 //Unbeatable AI
-
-
+//returns the position that the AI must use given certain conditions
+//returns the literal board position as int not the array posistion
+int unbeatable_ai(char current_board[8])
+{
+    
+    if (current_board[0] == current_board[1] && !position_taken(current_board, 3))
+    {
+        return 3;
+    }
+    else if (current_board[2] == current_board[1] && !position_taken(current_board, 1))
+    {
+        return 1;
+    }
+    else if (current_board[0] == current_board[2] && !position_taken(current_board, 2))
+    {
+        return 2;
+    }
+    else if (current_board[3] == current_board[4] && !position_taken(current_board, 6))
+    {
+        return 6;
+    }
+        
+        
+    return 5;
+    
+}
 //User Intro
+void user_intro()
+{
+    
